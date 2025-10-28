8675318a227150dd1d43760fdb4d5a0d403a2702
luisSun <luis.sun@gmail.com>
2025-10-28 09:14:50 -0300

    Primeiro Commit do Branch CICD

--- tests/teste01.spec.js
+++ tests/teste01.spec.js
@@ -2,12 +2,37 @@
 import { test, expect } from '@playwright/test';
 
 test.use({
-  viewport: {width: 1600, height: 1200}
+  // Context geolocation
+  geolocation: { longitude: 12.492507, latitude: 41.889938 },
+  permissions: ['geolocation'],
+  viewport: { width: 800, height: 600 }
 })
 
-test('Login com sucesso', async ({ page }) => {
-
+test.beforeEach(async({page}) =>{
   await page.goto('https://automationpratice.com.br/')
+})
+
+test('Login com sucesso @testLogin', async ({ page }) => {
+
+  const buttonEmail = await page.getByRole('button', { name: 'Send Mail' })
+  await buttonEmail.scrollIntoViewIfNeeded()
+  await buttonEmail.click()
+
+  const text = await page.waitForSelector('text=NEWSLETTER')
+  await text.scrollIntoViewIfNeeded()
+  
+  
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('a@a.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('senha@123');
+  await page.getByRole('button', { name: 'login' }).click();
+
+});
+
+
+test('Login com sucesso 2 @testLogin', async ({ page }) => {
 
   await page.getByRole('link', { name: ' Login' }).click();
   await page.locator('#user').click();
@@ -17,3 +42,20 @@ test('Login com sucesso', async ({ page }) => {
   await page.getByRole('button', { name: 'login' }).click();
 
 });
+
+test('Login com sucesso 3 @testLogin', async ({ page }) => {
+
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('a@a.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('senha@123');
+  await page.getByRole('button', { name: 'login' }).click();
+
+});
+
+
+test.afterEach(async({page})=>{
+  //Fazer algo depois de cada teste rodado
+  console.log("Vai Corinthians")
+})
